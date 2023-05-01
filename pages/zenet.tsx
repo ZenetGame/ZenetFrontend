446a505579b4fea9c3896c1df885ee8f2803e693
Alp Guneysel <alp.guneysel@gmail.com>
2023-05-01 03:38:59 -0400

    connect wallet and move me transaction POC

--- pages/zenet.tsx
+++ pages/zenet.tsx
@@ -1,20 +1,27 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
-import React, { useState, useEffect, useCallback, FC, useMemo } from "react";
+import React, { useState, useEffect, useCallback, FC, useMemo,useRef } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 import {useDevicePixelRatio} from 'use-device-pixel-ratio'
 
 import { WalletProvider } from "@demox-labs/aleo-wallet-adapter-react";
-import { WalletModalProvider } from "@demox-labs/aleo-wallet-adapter-reactui";
+import { WalletModalProvider, useWalletModal } from "@demox-labs/aleo-wallet-adapter-reactui";
 import { LeoWalletAdapter } from "@demox-labs/aleo-wallet-adapter-leo";
-import {
-  DecryptPermission,
-  WalletAdapterNetwork,
-  WalletNotConnectedError
-} from "@demox-labs/aleo-wallet-adapter-base";
 
 import { useWallet } from "@demox-labs/aleo-wallet-adapter-react";
+
+import {
+  DecryptPermission,
+  Transaction,
+  WalletAdapterNetwork,
+  WalletNotConnectedError,
+  MessageSignerWalletAdapter
+} from "@demox-labs/aleo-wallet-adapter-base";
+
+
+
+
 require("@demox-labs/aleo-wallet-adapter-reactui/styles.css");
 
 
@@ -22,9 +29,29 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
+
+  const { requestTransaction } = useWallet();
+
+
+    
+    const [isGameOver, setIsGameOver] = useState(false);
+    const [userName, setUserName] = useState();
+    const [move, setMove] = useState();
+    const [wallet, setWallet] = useState();
+    const [publicKey, setPublickey] = useState();
+
+
+
+
+
     const dpr = useDevicePixelRatio()
     console.log("DPR___",dpr);
-    const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
+
+
+
+
+
+    const { unityProvider, isLoaded, loadingProgression, addEventListener, removeEventListener } = useUnityContext({
         loaderUrl: "build/senet.loader.js",
         dataUrl: "build/senet.data",
         frameworkUrl: "build/senet.framework.js",
@@ -37,21 +64,67 @@ export default function Home() {
       );
     
 
-      function connectWallet(){
+      const handleGameOver = useCallback((userName, score) => {
+console.log("GAME_OVER")      
+}, []);
+
+      const handleSetMove = useCallback(async (position, move) => {
+        console.log('GAME_EVENT / SETMOVE: ', position,move);
+      
+
+
+
+
+
+      }, []);
+
+      const handleConnectWallet = useCallback(async () => {
+
+        console.log("HANDLE_CONNECT_WALLET");
         console.log("_______CONNECTING TO WALLET_______");
-          var wallet = new window.LeoWallet.LeoWalletAdapter({ appName: 'Vanilla JS Example'});
-          wallet.connect(window.AleoWalletBase.DecryptPermission.AutoDecrypt, window.AleoWalletBase.WalletAdapterNetwork.Testnet).then(() => {
-            let utf8Encode = new TextEncoder();
-            let bytes = utf8Encode.encode("Leo is awesome");
-            wallet.signMessage(bytes);
-            console.log('Signature: ', bytes);
-          });
-        
-        
-        
-        }
 
 
+        var wallet = new LeoWalletAdapter({ appName: 'Zenet'});
+
+
+        wallet.connect(DecryptPermission.AutoDecrypt, WalletAdapterNetwork.Testnet).then(() => {
+          let utf8Encode = new TextEncoder();
+          let bytes = utf8Encode.encode("Leo & Zenet are awesome");
+          wallet.signMessage(bytes);
+          console.log('Signature: ', bytes);
+          console.log('GAME_EVENT / PUBLICKEY: ', wallet.publicKey);
+        });
+        setPublickey(wallet.publicKey);
+        setWallet(wallet);
+      }, []);
+
+
+
+        
+          useEffect(() => {
+            addEventListener("GameOver", handleGameOver);
+            return () => {
+              removeEventListener("GameOver", handleGameOver);
+            };
+          }, [addEventListener, removeEventListener, handleGameOver]);
+          
+      
+     
+          useEffect(() => {
+            addEventListener("SetMove", handleSetMove);
+            return () => {
+              removeEventListener("SetMove", handleSetMove);
+            };
+          }, [addEventListener, removeEventListener, handleSetMove]);
+          
+      
+          useEffect(() => {
+            addEventListener("ConnectWallet", handleConnectWallet);
+            return () => {
+              removeEventListener("ConnectWallet", handleConnectWallet);
+            };
+          }, [addEventListener, removeEventListener, handleConnectWallet]);
+          
 
         function game_start(){
             console.log("_______START WALLET_______");
@@ -62,6 +135,9 @@ export default function Home() {
         }
         function game_move(){
             console.log("_______MOVE WALLET_______");
+
+
+            //leo run move 7u8 2u8
         }
 
 
@@ -75,7 +151,7 @@ export default function Home() {
             setDevicePixelRatio(dpr);
           };
           // A media matcher which watches for changes in the device pixel ratio.
-          const mediaMatcher = window.matchMedia(
+          const mediaMatcher = matchMedia(
             `screen and (resolution: ${devicePixelRatio}dppx)`
           );
           // Adding an event listener to the media matcher which will update the
@@ -92,20 +168,12 @@ export default function Home() {
 
   return (
 
-    <WalletProvider
-    wallets={wallets}
-    decryptPermission={DecryptPermission.UponRequest}
-    network={WalletAdapterNetwork.Localnet}
-    autoConnect
-  >
-    <WalletModalProvider>
         <Unity
             unityProvider={unityProvider}
-            style={{ width: 800, height: 600 }}
+            style={{ width: '100%', height: '100%' }}
             devicePixelRatio={devicePixelRatio}
         />
-    </WalletModalProvider>
-  </WalletProvider>
+
   )
 }
 
